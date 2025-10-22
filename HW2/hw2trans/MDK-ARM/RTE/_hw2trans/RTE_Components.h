5c3cd07d32c5ebdf0f1447409f8dfd245679817b
jwwww007 <wanjiarui0923@gmail.com>
2025-10-22 14:44:35 +0800

    添加 lesson3 作业

---
+++ HW2/hw2trans/MDK-ARM/RTE/_hw2trans/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'hw2trans' 
+ * Target:  'hw2trans' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
